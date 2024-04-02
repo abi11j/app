5d0d1cbf07eb1a69865a1014ab84017af014a44d
abi11j <164549444+abi11j@users.noreply.github.com>
2024-04-02 23:51:25 +0200

    Update useLogin.jsx

--- client/src/hooks/useLogin.jsx
+++ client/src/hooks/useLogin.jsx
@@ -10,7 +10,7 @@ export const useLogin = () => {
     setIsLoading(true);
     setError(null);
 
-    const res = await fetch("http://localhost:5000/api/user/login", {
+    const res = await fetch("https://app-api-sable.vercel.app/api/user/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ email, password }),
